daeef98ac4636b137deaeb7c17388454b64eb540
Andrey <60440684+Fox216540@users.noreply.github.com>
2026-03-12 01:15:54 +0100

    Change SSH remote port forwarding to localhost

--- gost-tunnel.sh
+++ gost-tunnel.sh
@@ -4,4 +4,4 @@ set -e
 # Проброс SOCKS5 через SSH c использованием gost
 PORT="${1:-1080}"
 
-exec gost -L "socks5://127.0.0.1:${PORT}" ssh -R "0.0.0.0:${PORT}:127.0.0.1:${PORT}" root@217.154.97.70
+exec gost -L "socks5://127.0.0.1:${PORT}" ssh -R "127.0.0.1:${PORT}:127.0.0.1:${PORT}" root@217.154.97.70
